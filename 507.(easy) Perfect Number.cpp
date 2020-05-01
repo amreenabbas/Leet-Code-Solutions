e83080da552215f7ae81dbcc404b16f0d5276a09
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-02 00:50:50 +0530

    Create  507.(easy) Perfect Number.cpp

---
+++ 507.(easy) Perfect Number.cpp
@@ -0,0 +1,35 @@
+/*
+We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+
+Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+Example:
+Input: 28
+Output: True
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+Note: The input number n will not exceed 100,000,000. (1e8)
+
+*/
+
+
+
+//created by js0805
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+      if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+                cout << sum << " ";
+
+            }
+        }
+        return sum - num == num;
+    }
+};
