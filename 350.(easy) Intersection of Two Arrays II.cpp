3a0116cc474f6e6ccd336b9ddf0439b9bed6e38a
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-06 01:38:00 +0530

    Create 350.(easy) Intersection of Two Arrays II.cpp

---
+++ 350.(easy) Intersection of Two Arrays II.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/
+//created by js0805
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++)
+        {
+            mp[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            
+            if(mp.find(nums2[i])!=mp.end())
+            {
+                if(mp[nums2[i]]>0)
+                {
+                    ans.push_back(nums2[i]);
+                    mp[nums2[i]]--;
+                }
+            }
+        }
+        return ans;
+            
+            
+        
+    }
+};
