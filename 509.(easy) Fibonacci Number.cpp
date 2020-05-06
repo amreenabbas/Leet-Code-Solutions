90e7691f5fe1388ad0390517ac5bd56bb5c0732a
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-07 00:54:47 +0530

    Create 509.(easy) Fibonacci Number.cpp

---
+++ 509.(easy) Fibonacci Number.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/fibonacci-number/
+//created by js0805
+class Solution {
+public:
+    int fib(int N) {
+        long long int t = (pow(2,N)*2.2360679775);
+        long long int a = (pow(1+2.2360679775,N));
+        long long int b = (pow(1-2.2360679775,N));
+        return (a-b)/t;
+    }
+};
