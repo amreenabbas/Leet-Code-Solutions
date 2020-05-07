dbd0144cef47db4f2cc491442ec2f3be1b1f0357
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-07 11:11:29 +0530

    Create 70. (easy)Climbing Stairs.cpp

---
+++ 70. (easy)Climbing Stairs.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/climbing-stairs/
+//created by js0805
+class Solution {
+public:
+        
+    int climbStairs(int N) {
+        long long int arr[N+1];
+        arr[0] = 1;
+        arr[1] = 1;
+       
+        for(int i =2;i<N+1;i++)
+        {
+            arr[i] = arr[i-1] + arr[i-2] ;
+        }
+        return arr[N];
+    }
+};
