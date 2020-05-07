0d81a433f2555a3cc5f0a282d5b5dab70ad57912
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-07 23:31:49 +0530

    Update 977.(easy) Squares of a Sorted Array.cpp

--- 977.(easy) Squares of a Sorted Array.cpp
+++ 977.(easy) Squares of a Sorted Array.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/squares-of-a-sorted-array/
 //created by js0805
+//naive-solution
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& A) {
@@ -9,5 +10,27 @@ public:
         }
         sort(A.begin(),A.end());
         return A;
+    }
+};
+//better solution would be to use inline and outline
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int>ans(A.size());
+        int r=A.size()-1,l=0;
+        int n = A.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(abs(A[r])>abs(A[l]))
+            {
+                ans[i] = A[r] *A[r--];
+            }
+            else
+            {
+                ans[i] = A[l] * A[l++];
+            }
+        }
+        return ans;
+        
     }
 };
