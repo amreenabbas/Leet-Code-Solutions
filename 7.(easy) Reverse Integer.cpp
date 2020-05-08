efdfcdc81bcc9175f17ff6a6067de7e24c711f2a
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-08 09:45:20 +0530

    Create 7.(easy) Reverse Integer.cpp

---
+++ 7.(easy) Reverse Integer.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/reverse-integer/
+//created by js0805
+class Solution {
+public:
+   int reverse(int x) {
+    long result = 0;
+    while(x != 0)
+    {
+        result = result*10 + x % 10;
+        x /= 10;
+    }
+    return (result > INT_MAX || result < INT_MIN)? 0 : result; // Here we are checking for int overflow.
+}
+        
+    
+    
+};
