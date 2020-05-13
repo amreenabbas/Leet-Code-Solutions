b381847f820795033e721f3975c18176cb381acd
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-13 10:31:19 +0530

    Update 11.(easy) Container With Most Water.cpp

--- 11.(easy) Container With Most Water.cpp
+++ 11.(easy) Container With Most Water.cpp
@@ -16,3 +16,29 @@ public:
         return sum;
     }
 };
+
+//created by js0805
+class Solution {
+public:
+   
+    int maxArea(vector<int>& height) {
+        int l =0,h = height.size()-1;
+        int sum =  0;
+        while(l<h)
+        {
+           int w = h-l;
+            if(height[l]<=height[h])
+            {
+                sum = max(sum,height[l]*w);
+                l++;
+            }
+            else
+            {
+                sum = max(sum,height[h]*w);
+                h--;
+            }
+           
+        }
+        return sum;
+    }
+};
