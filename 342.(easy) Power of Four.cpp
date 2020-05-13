95e97763a525ff4c74eea6bbab210d75747d7d75
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-14 00:21:07 +0530

    Create 342.(easy) Power of Four.cpp

---
+++ 342.(easy) Power of Four.cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/power-of-four/
+//created by js0805
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        
+           return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+    }
+};
