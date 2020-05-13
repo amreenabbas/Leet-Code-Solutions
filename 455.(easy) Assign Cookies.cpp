b7e7fd264ac36ce46cda7e8a8be1081e970c3556
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-14 00:51:57 +0530

    Create 455.(easy) Assign Cookies.cpp

---
+++ 455.(easy) Assign Cookies.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/assign-cookies/
+//created by js0805
+class Solution {
+public:
+int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0, j=0;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i])
+                i++; // when the child get the cookie, foward child by 1
+            j++;
+        }
+        return i;
+    }
+    
+};
