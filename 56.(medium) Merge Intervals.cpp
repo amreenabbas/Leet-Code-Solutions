f7037a95a0e83fd6d70e6601f8b7e7239f3002c6
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-13 11:41:31 +0530

    Create 56.(medium) Merge Intervals.cpp

---
+++ 56.(medium) Merge Intervals.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/merge-intervals/
+//created by js0805
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+         if(intervals.size()<=1) return intervals;
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back({intervals[0][0],intervals[0][1]});
+        int a =0;
+        for(int i =1;i<intervals.size();i++)
+        {
+            if(ans[a][1]<intervals[i][0])
+            {
+                a++;
+                ans.push_back({intervals[i][0],intervals[i][1]});
+            }
+            else if(ans[a][1] >= intervals[i][0])
+            {
+                ans[a][1] = max(ans[a][1],intervals[i][1]);
+            }
+        }
+        return ans;
+        
+    }
+};
+
