52d3da1e12ca58d8db3678556e3c035b1be79ca1
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-13 10:16:54 +0530

    Create 11.(easy) Container With Most Water.cpp

---
+++ 11.(easy) Container With Most Water.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/container-with-most-water/
+//created by js0805
+class Solution {
+public:
+   
+    int maxArea(vector<int>& height) {
+        int l =0,h = height.size()-1;
+        int sum =  0;
+        while(l<h)
+        {
+            int q = min(height[l],height[h]);
+            sum = max(sum,(h-l)*q);
+            while(height[l]<=q && l<h)l++;
+            while(height[h]<=q && l<h)h--;
+        }
+        return sum;
+    }
+};
