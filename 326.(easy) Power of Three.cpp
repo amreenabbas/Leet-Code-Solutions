c68ba74822430be566767f06b25110e5d4bec4cc
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-14 00:29:52 +0530

    Create 326.(easy) Power of Three.cpp

---
+++ 326.(easy) Power of Three.cpp
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/power-of-three/
+//created by js0805
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n>0 && 1162261467 % n ==0;
+    }
+};
