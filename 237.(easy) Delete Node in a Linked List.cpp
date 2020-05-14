28a96d2fd5857f5b928172cc801ed52a52555e84
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-14 20:33:49 +0530

    Create 237.(easy) Delete Node in a Linked List.cpp

---
+++ 237.(easy) Delete Node in a Linked List.cpp
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/delete-node-in-a-linked-list/
+//created by js0805
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        *node = *node->next;
+    }
+};
