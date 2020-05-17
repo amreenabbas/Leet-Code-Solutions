f44d8aa3b7a4fa9e19580347493ff1944a8cedc1
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-17 23:47:26 +0530

    Create 1441.(easy)Build an Array With Stack Operations.cpp

---
+++ 1441.(easy)Build an Array With Stack Operations.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/build-an-array-with-stack-operations/
+//created by js0805
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int j =0;
+        vector<string>ans;
+        for(int i =1;i<=n && j<target.size();i++)
+        {
+            //cout << check[i] <<"\n";
+            if(i==target[j])
+            {
+                ans.push_back("Push");
+                j++;
+            }
+            else
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+        }
+        return ans;
+    }
+};
