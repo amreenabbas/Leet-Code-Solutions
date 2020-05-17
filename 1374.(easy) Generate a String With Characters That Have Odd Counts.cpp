d35b6ba288d8b8d0781f4d2967edb49819cfb48f
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-17 15:56:11 +0530

    Create 1374.(easy) Generate a String With Characters That Have Odd Counts.cpp

---
+++ 1374.(easy) Generate a String With Characters That Have Odd Counts.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+//created by js0805
+class Solution {
+public:
+    string generateTheString(int n) {
+        string ans;
+        if(n==1)
+        {
+            ans.push_back('a');
+            return ans;
+        }
+        if(n%2==0)
+        {
+            for(int i =0;i<n-1;i++)
+            {
+                ans.push_back('a');
+            }
+            ans.push_back('b');
+        }
+        else{
+            int t = n-1;
+            for(int i =0;i<t-1;i++)
+            {
+                ans.push_back('a');
+            }
+            ans.push_back('b');
+            ans.push_back('c');
+            
+        }
+        return ans;
+    }
+};
