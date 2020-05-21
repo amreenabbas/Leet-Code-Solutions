bbc5585d8176a5a0110fda46dddca782738d53c7
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-22 00:54:52 +0530

    Create 392.(easy) Is Subsequence.cpp

---
+++ 392.(easy) Is Subsequence.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/is-subsequence/
+//created by js0805
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = t.length();
+        int q = s.length();
+        int j =0;
+        for(int i =0;i<n;i++)
+        {
+            if(t[i] == s[j])
+            {
+                j++;
+                cout << i <<" ";
+            }
+        }
+        if(j==q)
+        {
+            return true;
+        }
+        return false;
+    }
+};
