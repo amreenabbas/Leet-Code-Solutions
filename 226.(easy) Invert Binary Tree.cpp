a99dfe6fc5d620f6fe5721da73ccb2ff4c2b270b
Jatin Sharma <sjatin633@gmail.com>
2020-06-01 18:00:31 +0530

    created by js0805

---
+++ 226.(easy) Invert Binary Tree.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/invert-binary-tree/
+//created by js0805
+//simply using the concept of mirror
+//replacing left node with right and right with left using a temp node
+//and doing a post order traversal for the above operation.
+class Solution {
+public:
+	TreeNode* invertTree(TreeNode* root) {
+		if (root == NULL)return NULL;
+		invertTree(root->left);
+		invertTree(root->right);
+		auto temp = root->right;
+		root->right = root->left;
+		root->left = temp;
+		return root;
+	}
+};
