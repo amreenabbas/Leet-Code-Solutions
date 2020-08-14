07b12e800b972423b507218f1f9c2324a2435e4d
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-08-15 02:08:43 +0530

    Create 62.cpp

---
+++ 62.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/unique-paths/
+//created by js0805
+int dp[101][101] = {0};
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m==1 or n==1){
+            return 1;
+        }
+        if(dp[m][n]){
+            return dp[m][n];
+        }
+        return dp[m][n] = uniquePaths(m,n-1) + uniquePaths(m-1,n);
+        
+        
+        
+        
+    }
+};
