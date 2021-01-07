49f1741367b1a4262b2d457d793cb23533a5dce5
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-01-07 16:46:26 +0530

    Update and rename 53. (easy) Maximum subarray to 53.Maximum subarray.cpp

--- 53.Maximum subarray.cpp
+++ 53.Maximum subarray.cpp
@@ -1,12 +1,6 @@
-Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+Question Link : https://leetcode.com/problems/maximum-subarray/
 
-Example:
-
-Input: [-2,1,-3,4,-1,2,1,-5,4],
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
-
-
+//created by Amreen
 
 class Solution {
 public:
