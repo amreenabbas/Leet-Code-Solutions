9569bb4b1e972f53b61ffe2cf3fd6869d616a42f
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-01-07 16:51:01 +0530

    Update and rename 226.(easy) Invert Binary Tree.cpp to 226.Invert Binary Tree.cpp

--- 226.Invert Binary Tree.cpp
+++ 226.Invert Binary Tree.cpp
@@ -1,8 +1,10 @@
-//https://leetcode.com/problems/invert-binary-tree/
+Question Link : https://leetcode.com/problems/invert-binary-tree/
+
 //created by js0805
 //simply using the concept of mirror
 //replacing left node with right and right with left using a temp node
 //and doing a post order traversal for the above operation.
+
 class Solution {
 public:
 	TreeNode* invertTree(TreeNode* root) {
