ffc134baf233b6d4f742fe08b34bb0792bf78098
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-01-07 16:28:16 +0530

    Update and rename 217.(easy)Contains duplicate to 217.Contains duplicate.cpp

--- 217.Contains duplicate.cpp
+++ 217.Contains duplicate.cpp
@@ -1,23 +1,6 @@
-Given an array of integers, find if the array contains any duplicates.
+Question Link : https://leetcode.com/problems/contains-duplicate/
 
-Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
-
-Example 1:
-
-Input: [1,2,3,1]
-Output: true
-
-Example 2:
-
-Input: [1,2,3,4]
-Output: false
-
-Example 3:
-
-Input: [1,1,1,3,3,4,3,2,4,2]
-Output: true
-
-
+//Solution by Amreen
 
 class Solution {
 public:
@@ -36,6 +19,7 @@ public:
 };
 
 // Solution by js0805
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
