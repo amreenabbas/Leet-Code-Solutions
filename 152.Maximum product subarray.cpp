0dea16571b39ea9e5756857272ec52b209610936
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-01-07 16:18:04 +0530

    Update and rename 152. (medium) Maximum product subarray to 152.Maximum product subarray.cpp

--- 152.Maximum product subarray.cpp
+++ 152.Maximum product subarray.cpp
@@ -1,18 +1,7 @@
-Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+Question Link : https://leetcode.com/problems/maximum-product-subarray/
 
-Example 1:
-
-Input: [2,3,-2,4]
-Output: 6
-Explanation: [2,3] has the largest product 6.
-
-Example 2:
-
-Input: [-2,0,-1]
-Output: 0
-Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
-
-
+//created by Amreen
+ 
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
