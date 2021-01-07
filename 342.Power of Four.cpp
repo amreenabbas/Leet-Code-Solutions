6877c1a85c90f4763bdba19ed0cfcb24d17c816b
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-01-07 17:00:14 +0530

    Update and rename 342.(easy) Power of Four.cpp to 342.Power of Four.cpp

--- 342.Power of Four.cpp
+++ 342.Power of Four.cpp
@@ -1,5 +1,7 @@
-//https://leetcode.com/problems/power-of-four/
+Question Link : https://leetcode.com/problems/power-of-four/
+
 //created by js0805
+
 class Solution {
 public:
     bool isPowerOfFour(int num) {
