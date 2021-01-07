4f1b5f64829884631e6c322d686058ebf6e95b48
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-01-07 15:48:26 +0530

    Update 1025.(easy) Divisor Game.cpp

--- 1025.(easy) Divisor Game.cpp
+++ 1025.(easy) Divisor Game.cpp
@@ -1,6 +1,6 @@
 
-/*
-Alice and Bob take turns playing a game, with Alice starting first.
+Question Link : https://leetcode.com/problems/divisor-game/
+Question : Alice and Bob take turns playing a game, with Alice starting first.
 
 Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
 
@@ -27,7 +27,7 @@ Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
 Note:
 
 1 <= N <= 1000
-*/
+
 //created by js0805
 class Solution {
 public:
