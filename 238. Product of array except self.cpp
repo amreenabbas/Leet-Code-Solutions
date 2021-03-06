761f4ba5a06a8285b44f5062111b9c27d442dd36
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-03-06 22:54:45 +0530

    Create 238. Product of array except self.cpp

---
+++ 238. Product of array except self.cpp
@@ -0,0 +1,24 @@
+Question Link : https://leetcode.com/problems/product-of-array-except-self/
+
+//Solution by Amreen
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int prod = 1;
+        int i,n=nums.size();
+        vector<int>res;
+        for(i=0;i<n;i++){
+            res.push_back(prod);
+            prod*=nums[i];
+        }
+        prod = 1;
+        for(i=n-1;i>=0;i--)
+        {
+            res[i]*=prod;
+            prod*=nums[i];
+        }
+        return res;
+        
+    }
+};
