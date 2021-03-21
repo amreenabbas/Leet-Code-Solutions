b1ab70f286bc043e496acbed8390115242506148
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-03-21 16:14:44 +0530

    Update 3. Longest substring without repeating characters.cpp

--- 3. Longest substring without repeating characters.cpp
+++ 3. Longest substring without repeating characters.cpp
@@ -42,3 +42,30 @@ public:
         return maxl;
     }   
 };
+
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    int max(int a, int b)
+    {
+        return a>b?a:b;
+    }
+   
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        if(n==0)
+            return 0;
+        int i,j,maxl=0;
+        unordered_map<char,int>count;     
+        for(i=0,j=0;j<n;j++)
+        {
+            if(count.find(s[j])!=count.end())
+                i = max(i,count[s[j]]+1);
+            count[s[j]] = j;
+            maxl = max(maxl,j-i+1);
+        }
+        return maxl;
+    }   
+};
