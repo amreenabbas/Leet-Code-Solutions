95478159f7d7dfac1f908e216dce3f384279551f
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-06 13:33:07 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -23,3 +23,36 @@ public:
     }
 };
 
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    int binsearch(int l, int h, vector<int>&lis, int t)
+    {
+        while(l<h){
+            int m = l+(h-l)/2;
+            if(lis[m] >=t)
+                h=m;
+            else
+                l=m+1;
+        }
+        return h;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int i,j,n=nums.size(),l=1;
+        vector<int>lis(n,1);
+        lis[0] = nums[0];
+        for(i=1;i<n;i++)
+        {
+            if(nums[i]>lis[l-1])
+                lis[l++] = nums[i];
+            else
+            {
+                int ind = binsearch(0,l-1,lis,nums[i]);
+                lis[ind] = nums[i];
+            }
+            
+        }
+        return l;
+    }
+};
