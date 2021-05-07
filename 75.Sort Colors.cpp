c9a93d33d9d129fee2733d5627e8aef0a33fd713
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-07 19:11:44 +0530

    Update 75.Sort Colors.cpp

--- 75.Sort Colors.cpp
+++ 75.Sort Colors.cpp
@@ -22,3 +22,39 @@ public:
         }
     }
 };
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i,n=nums.size(),c0=0,c1=0,c2=0;
+        for(i=0;i<n;i++)
+        {
+            if(nums[i] == 0)
+                c0++;
+            if(nums[i] == 1)
+                c1++;
+            if(nums[i] == 2)
+                c2++;
+        }
+        for(i=0;i<n;i++)
+        {
+            if(c0)
+            {
+                nums[i] = 0;
+                c0--;
+            }
+            else if(c1)
+            {
+                nums[i] = 1;
+                c1--;
+            }
+            else if(c2)
+            {
+                nums[i] = 2;
+                c2--;
+            }
+        }
+    }
+};
