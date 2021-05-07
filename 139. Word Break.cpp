8e3006b82721e6666898aa7b22d65e1c9bf726e7
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-07 18:25:56 +0530

    Update 139. Word Break.cpp

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -30,3 +30,40 @@ public:
         return dp[l];
     }
 };
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string,int> dict;
+        int l = s.length();
+        vector<bool>dp(l+1,false);
+        vector<int>ind;
+        ind.push_back(-1);
+        if(l == 0)
+            return true;
+        int i,j,n=wordDict.size();
+        for(string word:wordDict)
+        {
+            dict[word]++;
+        }
+        dp[0] = true;
+        for(i=0;i<=l;i++)
+        {
+            int m = ind.size();
+            bool f = false;
+            for(j=m-1;j>=0;j--)
+            {
+                if(dict.find(s.substr(ind[j]+1,i-ind[j])) != dict.end())
+                {
+                    f=true;
+                    dp[i] = true;
+                    ind.push_back(i);
+                    break;
+                }
+            }
+        }
+        return dp[l];
+    }
+};
