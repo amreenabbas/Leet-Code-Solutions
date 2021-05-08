390b84f56eef57eff8182c12a34cc6045134240e
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-08 16:09:39 +0530

    Create 316. Remove Duplicate Letters.cpp

---
+++ 316. Remove Duplicate Letters.cpp
@@ -0,0 +1,37 @@
+Question Link : https://leetcode.com/problems/remove-duplicate-letters/
+
+//Solution by Amreen
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int c[26] = {0};
+        stack<char>st;
+        int i,n = s.length();
+        for(i=0;i<n;i++)
+            c[s[i]-'a']++;
+        vector<bool>vis(26,false);
+        for(char ch:s)
+        {
+            c[ch-'a']--;
+            if(!vis[ch-'a'])
+            {
+                while(!st.empty() && st.top()>ch && c[st.top()-'a']>0)
+                {
+                    vis[st.top()-'a'] = false;
+                    st.pop();
+                }
+                st.push(ch);
+                vis[ch-'a'] = true;
+            }
+            
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans = st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
