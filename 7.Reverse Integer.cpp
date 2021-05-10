e1ae3ccac7c7fc365178a3eec39edf9d45efe534
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-10 22:01:01 +0530

    Update 7.Reverse Integer.cpp

--- 7.Reverse Integer.cpp
+++ 7.Reverse Integer.cpp
@@ -13,7 +13,28 @@ public:
     }
     return (result > INT_MAX || result < INT_MIN)? 0 : result; // Here we are checking for int overflow.
 }
-        
-    
-    
+};
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    int reverse(int x) {
+        long rev=0,f=0;
+        if(x<0 && abs(x)>=INT_MAX)
+            return 0;
+        if(x<0)
+        {
+            f=1;
+            x=-x;
+        }   
+        while(x>0)
+        {
+            if(rev*10>=INT_MAX)
+                return 0;
+            rev = rev*10+(x%10);
+            x/=10;
+        }
+        return f?-(int)rev:(int)rev;
+    }
 };
