5e48da7361cb1caf22d3d09159d8882f52d8d219
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-18 15:50:13 +0530

    Update 226.Invert Binary Tree.cpp

--- 226.Invert Binary Tree.cpp
+++ 226.Invert Binary Tree.cpp
@@ -17,3 +17,18 @@ public:
 		return root;
 	}
 };
+
+//Alternate soluiton by Amreen
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+            return NULL;
+        TreeNode*l = invertTree(root->left);
+        TreeNode*r=invertTree(root->right);
+        root->left=r;
+        root->right = l;
+        return root;
+    }
+};
