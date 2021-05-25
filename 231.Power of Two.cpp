207fa3193d47af436b6e1746c9a742fb4bafa40c
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-25 21:24:14 +0530

    Update 231.Power of Two.cpp

--- 231.Power of Two.cpp
+++ 231.Power of Two.cpp
@@ -10,3 +10,26 @@ public:
         
     }
 };
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        long s=1;
+        int i;
+        for(i=1;i<=31;i++)
+        {
+            s*=2;
+            if(s==n)
+                return true;
+            if(s>n)
+                break;
+        }
+        return false;
+    }
+};
