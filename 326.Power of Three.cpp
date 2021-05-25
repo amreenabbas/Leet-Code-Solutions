7e1d69d7e4ddba0b74f39802a06681e588c148ea
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-25 21:21:16 +0530

    Update 326.Power of Three.cpp

--- 326.Power of Three.cpp
+++ 326.Power of Three.cpp
@@ -8,3 +8,27 @@ public:
         return n>0 && 1162261467 % n ==0;
     }
 };
+
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        long s=1;
+        int i;
+        for(i=1;i<=30;i++)
+        {
+            s*=3;
+            if(s==n)
+                return true;
+            if(s>n)
+                break;
+        }
+        return false;
+    }
+};
