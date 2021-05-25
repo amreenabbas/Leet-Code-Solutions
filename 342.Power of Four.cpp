9ebd979cdbc46fa55d5bd1ba8aee964f59f08eb9
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-05-25 21:26:31 +0530

    Update 342.Power of Four.cpp

--- 342.Power of Four.cpp
+++ 342.Power of Four.cpp
@@ -9,3 +9,26 @@ public:
            return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
     }
 };
+
+//Alternate solution by Amreen
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        long s=1;
+        int i;
+        for(i=1;i<=16;i++)
+        {
+            s*=4;
+            if(s==n)
+                return true;
+            if(s>n)
+                return false;
+        }
+        return false;
+    }
+};
