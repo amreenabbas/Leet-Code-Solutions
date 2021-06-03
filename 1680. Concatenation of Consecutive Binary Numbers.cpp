d88688a65ded059146f214fee50fa3f937141aa2
Amreen Abbas <50434514+amreenabbas@users.noreply.github.com>
2021-06-03 16:11:52 +0530

    Create 1680. Concatenation of Consecutive Binary Numbers.cpp

---
+++ 1680. Concatenation of Consecutive Binary Numbers.cpp
@@ -0,0 +1,17 @@
+Question Link : https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+
+//Solution by Amreen
+
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long res=0;
+        int i,mod=1000000007;
+        for(i=1;i<=n;i++)
+        {
+            int bits=(int)(log(i)/log(2)+1);
+            res=((res<<bits)|i)%mod;
+        }
+        return (int)res;
+    }   
+};
