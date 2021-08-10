3077c37655feccce153c974315303c0a01723d31
js0805 <sjatin633@gmail.com>
2021-08-10 18:32:33 +0530

    Added one more solution to 349

--- 349.Intersection of Two Arrays.cpp
+++ 349.Intersection of Two Arrays.cpp
@@ -23,3 +23,26 @@ public:
         return ans;
     }
 };
+
+//ALternate solution by js0805
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int arr[1001] = {0};
+        for(int i=0;i<nums1.size();i++){
+            arr[nums1[i]] = 1;
+        }
+        for(int i=0;i<nums2.size();i++){
+            if(arr[nums2[i]]==1){
+                arr[nums2[i]] = 2;
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<1001;i++){
+            if(arr[i]==2){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
