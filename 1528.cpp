90e37dee65f5d63280f8d858fb80889e788e16ef
js0805 <sjatin633@gmail.com>
2021-08-12 02:08:39 +0530

    added solution to 1528

---
+++ 1528.cpp
@@ -0,0 +1,11 @@
+//created by js0805
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+    string sol(s.length(),' ');
+      for(int i=0;i<indices.size();i++){
+          sol[indices[i]] = s[i];
+      }
+        return sol;
+    }
+};
